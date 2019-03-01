3d4b9812119c66f0fb3e1e3d9588ceeaf56beac2
Irving Duran <ijd.irving@gmail.com>
2019-03-01 16:14:57 -0400

    done with functions

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,13 +6,20 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-function consume(param1, param2, callback){ 
-  var same = param1 + param2 +"this is a call back ";
-  callback.push(same);
+// function consume(param1, param2, callback){ 
+//   var same = param1 + param2 +"this is a call back ";
+//   callback.push(same);
+// }
+
+// consume(2, 3);
+
+function consume(x, y, callback) {
+  return callback(x, y);
 }
 
-consume(2, 3);
-
+ consume(20, 2, (x, y) =>
+  console.log(`today is hard ${x} & ${y}`)
+);
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -21,16 +28,34 @@ consume(2, 3);
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function consume(param1, param2, callbackFunction) {
+  return callbackFunction(param1,param2);
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function greeting(firstname,lastname) {
+  return `Hello ${firstname} ${lastname}, nice to meet you!`;
+}
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
+
+//Because the function has a console.log in it which is calling out an internal function.
 
 // Explanation: 
 
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -99,7 +99,7 @@ const uni = [];
         uni.push(graduates[i]['university']);
     }
 }
-console.log(uni)
+console.log(uni);
 // ==== ADVANCED Array Methods ====
 
 // Given this zoo data from around the United States, follow the instructions below.  Use the specific array methods in the requests below to solve the problems.
